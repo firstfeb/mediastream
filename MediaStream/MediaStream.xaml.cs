36e143358d8acf71085d978f468cebe5a39cb8a6
Filip <filip.kantar@insm.se>
2019-05-01 00:22:03 +0200

    MediaStream template is now using WinForms version of Vlc.DotNet. Added MediaStreamer template that is using the WinForms version of LibVlcSharp.

---
+++ MediaStream/MediaStream.xaml.cs
@@ -0,0 +1,87 @@
+﻿using INSM.Template.Framework.v1;
+using System;
+using System.IO;
+using System.Windows.Forms.Integration;
+using Vlc.DotNet.Forms;
+
+namespace MediaStream
+{
+    public partial class MediaStream : ITemplateControl
+    {
+        private Template _mTemplate;
+        private const string KeyTvUrl = "TvUrl";
+        private WindowsFormsHost windowsFormsHost;
+        private VlcControl vlcControl;
+        private string streamUrl;
+
+        public MediaStream()
+        {
+            InitializeComponent();
+            windowsFormsHost = new WindowsFormsHost();
+            vlcControl = new VlcControl();
+
+            windowsFormsHost.Child = vlcControl;
+            VlcGrid.Children.Add(windowsFormsHost);
+
+            _mTemplate = new Template(new TemplateDataSet("Default template dataset", "",
+                new TemplateDataSetItem(TemplateDataSetItemType.Text, KeyTvUrl, "", ""
+                )));
+
+            _mTemplate.TemplateDataSet.TemplateDataSetChanged += TemplateDataSet_TemplateDataSetChanged;
+            _mTemplate.TemplatePlaying += m_Template_TemplatePlaying;
+            _mTemplate.TemplateStopped += m_Template_TemplateStopped;
+            _mTemplate.TemplatePaused += m_Template_TemplatePaused;
+            _mTemplate.TemplateUnload += m_Template_TemplateUnload;
+        }
+
+        private void TemplateDataSet_TemplateDataSetChanged(TemplateDataSetEventArgs e)
+        {
+            streamUrl = _mTemplate.TemplateDataSet.GetTemplateDataSetItemAsText(KeyTvUrl, string.Empty);
+            var templatePath = _mTemplate.Attributes["TemplatePath"];
+            var libPath = Path.Combine(templatePath, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64");
+            var libDirectory = new DirectoryInfo(libPath);
+
+            vlcControl.BeginInit();
+            vlcControl.VlcLibDirectory = libDirectory;
+            vlcControl.EndInit();
+        }
+
+        private void m_Template_TemplatePlaying(object sender, PlayingEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(streamUrl) && !vlcControl.IsPlaying)
+            {
+                vlcControl.Play(new Uri(streamUrl));
+            }
+        }
+
+        private void m_Template_TemplateStopped(object sender, StoppedEventArgs e)
+        {
+            if (vlcControl.IsPlaying)
+            {
+                vlcControl.Stop();
+            }
+        }
+
+        private void m_Template_TemplatePaused(object sender, PausedEventArgs e)
+        {
+            if (vlcControl.IsPlaying)
+            {
+                vlcControl.Pause();
+            }
+        }
+
+        private void m_Template_TemplateUnload(object sender, UnloadEventArgs e)
+        {
+            _mTemplate.TemplateDataSet.TemplateDataSetChanged -= TemplateDataSet_TemplateDataSetChanged;
+            _mTemplate.TemplatePlaying -= m_Template_TemplatePlaying;
+            _mTemplate.TemplateStopped -= m_Template_TemplateStopped;
+            _mTemplate.TemplateUnload -= m_Template_TemplateUnload;
+            _mTemplate.TemplatePaused -= m_Template_TemplatePaused;
+        }
+
+        public ITemplate TemplateInstance
+        {
+            get { return _mTemplate; }
+        }
+    }
+}
---
+++ MediaStreamer/MediaStreamer.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+using INSM.Template.Framework.v1;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+
+namespace MediaStreamer
+{
+
+    public partial class MediaStreamer : ITemplateControl
+    {
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+        private Template _template;
+        private const string KeyTvUrl = "TvUrl";
+
+        private WindowsFormsHost _host = new WindowsFormsHost();
+        private VideoView _videoView;
+
+        private string _streamUrl = string.Empty;
+
+        public MediaStreamer()
+        {
+            Core.Initialize();
+
+            InitializeComponent();
+
+            _template = new Template(new TemplateDataSet("Default template dataset", "",
+                new TemplateDataSetItem(TemplateDataSetItemType.Text, KeyTvUrl, "", ""
+                )));
+
+            _template.TemplateDataSet.TemplateDataSetChanged += TemplateDataSet_TemplateDataSetChanged;
+            _template.TemplatePlaying += _template_TemplatePlaying;
+            _template.TemplateStopped += _template_TemplateStopped;
+            _template.TemplatePaused += _template_TemplatePaused;
+            _template.TemplateUnload += _template_TemplateUnload;
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+            _videoView = new VideoView();
+            _videoView.MediaPlayer = _mediaPlayer;
+            _host.Child = _videoView;
+
+            VlcGrid.Children.Add(_host);
+        }
+
+        private void TemplateDataSet_TemplateDataSetChanged(TemplateDataSetEventArgs e)
+        {
+            _streamUrl = _template.TemplateDataSet.GetTemplateDataSetItemAsText(KeyTvUrl, string.Empty);
+        }
+
+        private void _template_TemplatePlaying(object sender, PlayingEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_streamUrl) && !_mediaPlayer.IsPlaying)
+            {
+                Media media = new Media(_libVLC, _streamUrl, FromType.FromLocation);
+                _mediaPlayer.Play(media);
+            }
+        }
+
+        private void _template_TemplateStopped(object sender, StoppedEventArgs e)
+        {
+            if (_mediaPlayer.IsPlaying)
+            {
+                _mediaPlayer.Stop();
+            }
+        }
+
+        private void _template_TemplatePaused(object sender, PausedEventArgs e)
+        {
+            if (_mediaPlayer.IsPlaying)
+            {
+                _mediaPlayer.Pause();
+            }
+        }
+
+        private void _template_TemplateUnload(object sender, UnloadEventArgs e)
+        {
+            _template.TemplateDataSet.TemplateDataSetChanged -= TemplateDataSet_TemplateDataSetChanged;
+            _template.TemplatePlaying -= _template_TemplatePlaying;
+            _template.TemplateStopped -= _template_TemplateStopped;
+            _template.TemplatePaused -= _template_TemplatePaused;
+            _template.TemplateUnload -= _template_TemplateUnload;
+        }
+
+        public ITemplate TemplateInstance
+        {
+            get { return _template; }
+        }
+    }
+}
